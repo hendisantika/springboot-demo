7ab21582612668e8ce054f7e7b54bb2185f4b624
Hendi Santika <hendisantika@gmail.com>
2019-12-29 06:26:14 +0700

    #5 - Add Person Service

---
+++ src/main/java/service/PersonService.java
@@ -0,0 +1,18 @@
+package service;
+
+import com.hendisantika.springboot2demo.model.Person;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot2-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 29/12/19
+ * Time: 06.25
+ */
+public interface PersonService {
+    Person johnSmith();
+
+    String hello(Person person);
+}
