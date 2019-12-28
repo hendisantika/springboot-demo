112e703957830add5dddc4a50fab75542b845383
Hendi Santika <hendisantika@gmail.com>
2019-12-29 06:20:43 +0700

    #1 - Initial commit

---
+++ src/test/java/com/hendisantika/springboot2demo/SpringBoot2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springboot2demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
