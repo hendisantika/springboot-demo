8dde984b752e0b7483efc8e17db3233ff4a9bafe
Hendi Santika <hendisantika@gmail.com>
2019-12-29 06:29:21 +0700

    #11 - Add DemoException postHello in PersonController

---
+++ src/main/java/com/hendisantika/springboot2demo/exception/DemoException.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springboot2demo.exception;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot2-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 29/12/19
+ * Time: 06.29
+ */
+public class DemoException extends Exception {
+}
