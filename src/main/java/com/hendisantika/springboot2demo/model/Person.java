29d19791c31d91e6bee8995eb97caa9622143bee
Hendi Santika <hendisantika@gmail.com>
2019-12-29 06:25:33 +0700

    #4 - Add Person model

---
+++ src/main/java/com/hendisantika/springboot2demo/model/Person.java
@@ -0,0 +1,17 @@
+package com.hendisantika.springboot2demo.model;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot2-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 29/12/19
+ * Time: 06.25
+ */
+@Data
+public class Person {
+    String firstname, lastname;
+}
